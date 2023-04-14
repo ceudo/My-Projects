1b917dc3f50fbc238d02476ba0dd560f5f791e23
ceudo <130686361+ceudo@users.noreply.github.com>
2023-04-14 23:10:14 +0800

    Update and rename Game.java to Game_coin.java

--- Game_coin.java
+++ Game_coin.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.Arrays;
-public class Game{
+public class Game_coin{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Hey, what is your name?");
